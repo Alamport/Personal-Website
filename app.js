1eea4405669b421d7587abb82b7a531260e3d757
Adin Lamport <adin.lamport@verizon.net>
2025-06-30 17:55:12 +0900

    Updated on the side icon, making it dynamic with javascript

--- app.js
+++ app.js
@@ -7,3 +7,21 @@ function myFunction() {
     x.style.display = "block";
   }
 }
+
+// function myFunction() {
+//     const sidebar = document.getElementById("myLinks");
+//     if (sidebar.style.width === "250px") {
+//         sidebar.style.width = "0";
+//     } else {
+//         sidebar.style.width = "250px";
+//     }
+// }
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
