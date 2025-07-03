f718d03e30a8db50899aca4939c4d590168234ae
Adin Lamport <adin.lamport@verizon.net>
2025-07-03 14:19:14 +0900

    Dynamical Changes + Font Styling
    • Added font family styling
    • Made Projects dynamic for mobile
    • Made nav dynamic for mobile

--- app.js
+++ app.js
@@ -8,14 +8,22 @@ function myFunction() {
   }
 }
 
-/* Set the width of the side navigation to 250px */
+// /* Set the width of the side navigation to 250px */
+// function openNav() {
+//   document.getElementById("mySidenav").style.width = "250px";
+// }
+
+// /* Set the width of the side navigation to 0 */
+// function closeNav() {
+//   document.getElementById("mySidenav").style.width = "0";
+// }
+
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("mySidenav").classList.add("open");
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
-  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("mySidenav").classList.remove("open");
 }
 
 // document.addEventListener('DOMContentLoaded', () => {
